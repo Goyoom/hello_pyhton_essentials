10d5fc13a5a24a89f7961cc86c97c7df03c4262e
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2020-07-23 21:40:13 +0300

    initial commit

---
+++ session_200718.py
@@ -0,0 +1,22 @@
+# ------------------------------------------------------------------------------
+# (c) 2020 Goyoom Education.
+# July 18, 2020
+# ------------------------------------------------------------------------------
+
+# TODO: [x] What is Python?
+# TODO: [x] Python vs. Java
+# TODO: [x] Interpreter vs. Compiler
+# TODO: [x] Python Interactive Mode
+# TODO: [x] The '_' read-only variable
+# TODO: [x] Duck Typing
+
+# TODO: [x] Comments in Python
+# This is a comment
+# There is no multi-line comment in Python
+
+# Inline comment
+names = [
+    'John',  # John Watson
+    'Peter',  # Peter Eamon
+    'Sarah'  # Sarah Anderson
+]
---
+++ session_200723.py
@@ -0,0 +1,17 @@
+#!venv/bin/python
+# ------------------------------------------------------------------------------
+# (c) 2020 Goyoom Education.
+# July 23, 2020
+# ------------------------------------------------------------------------------
+
+# TODO: [x] The Shebang Line
+# TODO: [x] Using 'Pylint'
+# TODO: [x] Requirements
+# TODO: [x] Local History and Labels
+# TODO: [x] Git Repository
+
+# moved to e the next session
+# TODO: [] Using the .gitignore file
+# TODO: [] Pip install -r requirements.txt
+# TODO: [] Docstring
+# TODO: [] The Zen of Python
