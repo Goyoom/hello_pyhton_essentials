3dc18ece786b578a15b050ea52b4891ac75078fd
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2020-09-05 14:59:22 +0300

    Workshop September 5, 2020

--- session_200829.py
+++ session_200829.py
@@ -7,7 +7,7 @@
 
 Strings."""
 
-# Done: F-Strings
+# DONE: F-Strings
 number = 42
 log1 = f'Employee No.{number}'
 log2 = f'Employee No.{number:03d}'
@@ -15,18 +15,18 @@ log2 = f'Employee No.{number:03d}'
 print(log1)
 print(log2)
 
-# Done: F-Strings Conversions
+# DONE: F-Strings Conversions
 name = 'John'
 
 print(f'His name is {name!s}')  # His name is John (default)
 print(f'His name is {name!r}')  # His name is 'John' (equal to His name is name.__repr__())
 print(name.__repr__())  # 'John'
 
-# Done: Characters Escaping
+# DONE: Characters Escaping
 print("His name is 'John'")  # His name is 'John'
 print('His name\nis \'John\'')  # His name is 'John'
 
-# Done: R-Strings
+# DONE: R-Strings
 print(r'C:\path\to\file')  # C:\path\to\file
 
 # r-strings cannot end with '\' backslash, to solve this:
@@ -34,7 +34,7 @@ print(r'C:\path\to\file' '\\')  # C:\path\to\file\
 print(r'C:\path\to\file\ '[:-1])  # C:\path\to\file\
 # print('C:\path\\to\\file\\')  # C:\path\to\file\ (less readable)
 
-# Done: Combining F-Strings With R-Strings
+# DONE: Combining F-Strings With R-Strings
 ext = 'jpg'
 
 print(fr'C:\path\to\file.{ext}')  # C:\path\to\file.jpg
